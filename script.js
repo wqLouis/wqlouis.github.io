0189063b4a41a3ac4e3fc19ad14ed5026c49f1ad
LouisW <wongqiluo@gmail.com>
2024-12-15 17:23:10 +0800

    Added manu and some js

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const checkbox = document.getElementsByClassName("manu-check")
-const manu = document.getElementsByClassName("manu")
-
-
-if (checkbox.chekced) {
-    manu
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var checkbox = document.querySelector('.manu-check');
+    var manu = document.querySelector(".manu");
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            manu.classList.add("show");
+        }
+        else {
+            manu.classList.remove("show");
+        }
+    });
+    window.onresize = function () {
+        if (window.innerWidth > 660) { //660 is from nav-bar.css
+            checkbox.checked = false;
+            manu.classList.remove("show");
+        }
+    };
+});
