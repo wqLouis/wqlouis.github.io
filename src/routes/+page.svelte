4691022fccd67748c6090f3e14623e1ad6e8ba51
wqlouis <wongqiluo@gmail.com>
2025-11-13 10:42:19 +0800

    Added language in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { smooth_scroll } from '../lib/smooth_scroll.ts';
 	smooth_scroll();
+
+	let langs = ['Python', 'C++', 'C', 'C#', 'Rust', 'TypeScript', 'JavaScript'];
 </script>
 
 <section></section>
@@ -21,8 +23,19 @@
 			</div>
 			<div class="flex-auto border-y border-dashed border-slate-700"></div>
 		</div>
-		<div class="flex h-[80vh] w-full border-b border-slate-700">
-			<div class="m-auto">hello</div>
+		<div class="h-40 w-full border-b border-slate-700">
+			<div class="mt-8 ml-8 text-2xl">Languages</div>
+			<div class="grid place-content-center">
+				<div class="flex overflow-x-auto pt-8 pb-11 whitespace-nowrap" id="lang-card">
+					{#each langs as lang}
+						<div
+							class="mx-4 max-w-32 min-w-32 flex-auto border border-slate-700 bg-slate-900 p-4 text-center transition-all text-shadow-slate-50 hover:mx-16 hover:scale-150"
+						>
+							{lang}
+						</div>
+					{/each}
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
