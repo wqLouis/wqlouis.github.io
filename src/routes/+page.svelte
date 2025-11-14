70037474dd795efc838f9a4f5aafe582e77eebf2
wqlouis <wongqiluo@gmail.com>
2025-11-14 15:56:07 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { smooth_scroll } from '../lib/smooth_scroll.ts';
 	smooth_scroll();
 
-	let langs = ['Python', 'C++', 'C', 'C#', 'Rust', 'TypeScript', 'JavaScript'];
+	const langs = ['Python', 'C++', 'C', 'C#', 'Rust', 'TS & JS'];
+	const langsLibs = [
+		'OpenCV llamacpp flet sqlite chromadb numpy',
+		'Qt6',
+		'Nothing here yet ;)',
+		'Unity game engine',
+		'rocket axum',
+		'Svelte',
+		''
+	];
 
-	const fadeArr = (arr: Array<string>) => {
-		var new_arr: Array<{ val: string; opa: string }> = [];
-		arr.forEach((value, idx, list) => {
-			new_arr.push({
-				val: value,
-				opa: (idx / list.length).toString()
+	onMount(() => {
+		var cards = Array.from((document.getElementById('lang-card') as HTMLDivElement).children);
+		var cardLib = document.getElementById('lang-libs') as HTMLElement;
+		var focus_card = 6;
+
+		cards.forEach((val, idx, arr) => {
+			var cardEle = val as HTMLElement;
+
+			cardEle.addEventListener('mouseenter', () => {
+				cardEle.classList.remove('opacity-30');
+				cardEle.classList.add('opacity-100');
+				focus_card = idx;
+				cardLib.innerHTML = langsLibs[focus_card];
+			});
+
+			cardEle.addEventListener('mouseout', () => {
+				cardEle.classList.add('opacity-30');
+				cardEle.classList.remove('opacity-100');
 			});
 		});
-		return new_arr;
-	};
-
-	const tech_stack_lang_cards = fadeArr(langs);
+	});
 </script>
 
 <section></section>
@@ -39,16 +58,35 @@
 		<div class="h-40 w-full border-b border-slate-700">
 			<div class="mt-8 ml-8 text-2xl">Languages</div>
 			<div class="grid place-content-center">
-				<div class="flex overflow-x-auto pt-8 pb-11 whitespace-nowrap" id="lang-card">
-					{#each tech_stack_lang_cards as lang}
+				<div class="flex overflow-x-auto px-12 pt-8 pb-11 whitespace-nowrap" id="lang-card">
+					{#each langs as lang}
 						<div
-							class="{lang.opa} mx-4 max-w-32 min-w-32 border border-slate-700 bg-slate-900 p-4 text-center transition-all text-shadow-slate-50 hover:mx-16 hover:scale-150"
+							class="mx-4 max-w-32 min-w-32 border border-slate-700 bg-slate-900 p-4 text-center opacity-30 transition-all text-shadow-slate-50 hover:z-1 hover:mx-0 hover:scale-150 hover:cursor-pointer"
 						>
-							{lang.val}
+							{lang}
 						</div>
 					{/each}
 				</div>
 			</div>
 		</div>
+		<div
+			class="h-24 w-full border-b border-slate-700 py-8 pl-8 text-xl text-slate-50"
+			id="lang-libs"
+		></div>
 	</div>
 </main>
+
+<style>
+	#lang-libs {
+		animation: rolling 1000ms;
+	}
+
+	@keyframes rolling {
+		from {
+			transform: translateX(-100vw);
+		}
+		to {
+			transform: translateX(100vw);
+		}
+	}
+</style>
